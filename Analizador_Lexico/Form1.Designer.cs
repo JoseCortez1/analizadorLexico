6042dcfc2334ae3e88894b3645b25b6c8e272c04
JoseCortez1 <50307754+JoseCortez1@users.noreply.github.com>
2019-05-13 15:08:04 -0500

    Add files via upload

---
+++ Analizador_Lexico/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Analizador_Lexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cadenaEntrada = new System.Windows.Forms.TextBox();
+            this.Componentes = new System.Windows.Forms.TextBox();
+            this.Estados = new System.Windows.Forms.TextBox();
+            this.Tokens = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnAnalizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAnalizar.Location = new System.Drawing.Point(55, 575);
+            this.btnAnalizar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(125, 33);
+            this.btnAnalizar.TabIndex = 0;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = false;
+            this.btnAnalizar.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.Location = new System.Drawing.Point(253, 575);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cadena";
+            // 
+            // cadenaEntrada
+            // 
+            this.cadenaEntrada.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cadenaEntrada.Location = new System.Drawing.Point(270, 78);
+            this.cadenaEntrada.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.cadenaEntrada.Name = "cadenaEntrada";
+            this.cadenaEntrada.Size = new System.Drawing.Size(956, 27);
+            this.cadenaEntrada.TabIndex = 3;
+            this.cadenaEntrada.TextChanged += new System.EventHandler(this.CadenaEntrada_TextChanged);
+            // 
+            // Componentes
+            // 
+            this.Componentes.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Componentes.Location = new System.Drawing.Point(55, 176);
+            this.Componentes.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Componentes.Multiline = true;
+            this.Componentes.Name = "Componentes";
+            this.Componentes.Size = new System.Drawing.Size(562, 219);
+            this.Componentes.TabIndex = 4;
+            // 
+            // Estados
+            // 
+            this.Estados.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Estados.Location = new System.Drawing.Point(663, 176);
+            this.Estados.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Estados.Multiline = true;
+            this.Estados.Name = "Estados";
+            this.Estados.Size = new System.Drawing.Size(562, 219);
+            this.Estados.TabIndex = 5;
+            // 
+            // Tokens
+            // 
+            this.Tokens.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tokens.Location = new System.Drawing.Point(55, 451);
+            this.Tokens.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Tokens.Multiline = true;
+            this.Tokens.Name = "Tokens";
+            this.Tokens.Size = new System.Drawing.Size(1171, 94);
+            this.Tokens.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 428);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tokens";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(658, 152);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Estados";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 152);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Componentes Lexicos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 593);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Tokens);
+            this.Controls.Add(this.Estados);
+            this.Controls.Add(this.Componentes);
+            this.Controls.Add(this.cadenaEntrada);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAnalizar);
+            this.Font = new System.Drawing.Font("Bodoni MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cadenaEntrada;
+        private System.Windows.Forms.TextBox Componentes;
+        private System.Windows.Forms.TextBox Estados;
+        private System.Windows.Forms.TextBox Tokens;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Analizador_Lexico/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizador_Lexico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void analizar()
+        {
+            string entrada = cadenaEntrada.Text;
+            bool digito = false, letra = false, guion = false;
+
+            if(entrada[entrada.Length -1] == ';')
+            {
+
+                if (entrada[0] == '-')
+                {
+                    digito = true;
+                }
+                if (entrada[0] == '+')
+                {
+                    digito = true;
+                }
+                if (char.IsDigit(entrada[0]))
+                    digito = true;
+
+                if (digito == true){
+
+                    bool numeroNegativo = false, numeroExiste = false, punto = false, numeroError = false;
+
+                    if (entrada[0] == '-')
+                    {
+                        Estados.Text += "\n1, ";
+                        numeroNegativo = true;
+                    }
+                    else
+                    {
+                        Estados.Text += "\n1, ";
+                    }
+
+                    for (int contador = 1; contador < entrada.Length - 1; contador++) {
+                        if ((entrada[contador] == '.') == punto){
+                            numeroError = true;
+                        }
+
+                        if ((entrada[contador] == '-') == numeroNegativo)
+                            numeroError = true;
+
+                        if ((punto && (numeroExiste == false)) || (numeroNegativo && (numeroExiste == false)))
+                            numeroError = true;
+
+                        if (char.IsNumber(entrada[contador]) == !punto)
+                        {
+                            Estados.Text += "2, ";
+                            numeroExiste = true;
+                        }
+                        else if (char.IsNumber(entrada[contador]) == punto)
+                        {
+                            Estados.Text += "4, ";
+                        }
+
+                        if ((entrada[contador] == '.') == numeroExiste)
+                        {
+                            punto = true;
+                            Estados.Text += "4, ";
+                        }
+
+                    }
+
+                    if (numeroError)
+                    {
+                        Tokens.Text += " \nExpresion Valida\n";
+
+                        if (punto && numeroNegativo)
+                            Componentes.Text += "\n" + entrada.ToString() + ": Numero Real, Negativo\n";
+                        else if (punto && !numeroNegativo)
+                            Componentes.Text += "\n" + entrada.ToString() + ": Numero Real, Positivo\n";
+                        if (!punto && numeroNegativo)
+                            Componentes.Text += "\n" + entrada.ToString() + ": Numero Entero, Negativo\n";
+                        else if (!punto && !numeroNegativo)
+                        {
+                            Componentes.Text += "\n" + entrada.ToString() + ": Numero Entero, Positivo\n";
+                        }
+
+                        Estados.Text += " 3";
+                    }
+                    else if (numeroError)
+                    {
+                        Tokens.Text += " \n Expresion Invalida\n";
+                    }
+                }
+
+                /* Letras                      */
+
+
+                if ((entrada[0] == '_') || char.IsLetter(entrada[0]))
+                {
+                    bool expresionAritmetica=false;
+                    int inicio = 0;
+                    if (entrada[0] == '_')
+                    {
+                        Estados.Text += "15, ";
+                        Componentes.Text += entrada[0].ToString() + " <- Guion bajo    ";
+                        inicio = 1;
+                    }
+                    guion = true;
+                    letra = false;
+                    for (int contador = inicio; contador < entrada.Length - 1; contador++)
+                    {
+                        char[] opr = new char[] { '*', '/', '+', '-', '^' };
+                        bool operadorBool = false;
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if (entrada[contador] == opr[i])
+                            {
+                                operadorBool = true;
+                                Estados.Text += "11, ";
+                                Componentes.Text += opr[i].ToString() + "   <- Operador   ";
+                            }
+                            else if(char.IsLetter(entrada[contador]))
+                            {
+                                while ((char.IsLetter(entrada[contador])) || (char.IsDigit(entrada[contador])))
+                                {
+                                    if (char.IsLetter(entrada[contador]))
+                                    {
+                                        while (char.IsLetter(entrada[contador]))
+                                        {
+                                            Estados.Text += "6, ";
+                                            Componentes.Text += entrada[contador];
+                                            contador++;
+                                        }
+                                    }
+                                    if (char.IsDigit(entrada[contador]))
+                                    {
+                                        while (char.IsDigit(entrada[contador]))
+                                        {
+                                            Estados.Text += "14, ";
+                                            Componentes.Text += entrada[contador];
+                                            contador++;
+                                        }
+                                    }
+                                }
+                                Componentes.Text += "  <-Variable  ";
+
+                            }
+                            /*/                                                                 */
+                            if (char.IsDigit(entrada[contador]))
+                            {
+                                bool NotEntero = false;
+                                while (char.IsDigit(entrada[contador]))
+                                {
+                                    Estados.Text += "14, ";
+                                    Componentes.Text += entrada[contador];
+                                    contador++;
+
+                                    if(entrada[contador] == '.')
+                                    {
+                                        NotEntero = true;
+                                        Estados.Text += "10, ";
+                                        Componentes.Text += entrada[contador];
+                                        contador++;
+                                        while (char.IsDigit(entrada[contador]))
+                                        {
+                                            Estados.Text += "14, ";
+                                            Componentes.Text += entrada[contador];
+                                            contador++;
+                                        }
+                                        Componentes.Text += "  <-Real   ";
+                                    }
+                                }
+                                if (!NotEntero)
+                                {
+                                    Componentes.Text += "  <-Entero    ";
+                                }
+                                
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    Tokens.Text += entrada + " <- Variables y operaciones necesitan empezar con lentra o _ ;  ";
+                }
+            }
+            else
+            {
+                Tokens.Text += entrada + " <- Falta ;  ";
+            }
+        }
+
+        public bool operadores(string cadena,int posicion)
+        {
+            char[] opr = new char[] { '*', '/', '+', '-', '^' };
+            bool operadorBool = false;
+            for(int contador = posicion; contador < cadena.Length-2;contador++)
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    if (cadena[contador] == opr[i])
+                        operadorBool = true;
+                }
+                if (operadorBool)
+                {
+                    if (cadena[contador] == cadena[contador + 1])
+                        return true;
+                }
+                operadorBool = false;
+            }
+            return false;
+        }
+
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            analizar();
+            Tokens.Text += " \n";
+            Componentes.Text += "\n ";
+            cadenaEntrada.Text += "\n ";
+            Estados.Text += " \n";
+        }
+        public void error(string _cadena)
+        {
+            Tokens.Text += "Expresion no valida";
+            Componentes.Text += _cadena + "  -> entrada invalida  ";
+            cadenaEntrada.Text += " ";
+            Estados.Text += "Estado invalido ";
+        }
+        private void CadenaEntrada_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Tokens.Text = " ";
+            Componentes.Text = " ";
+            cadenaEntrada.Text = " ";
+            Estados.Text = " ";
+        }
+    }
+}
